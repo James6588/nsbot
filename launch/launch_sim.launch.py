71ff54aab81691012956e9265f4238f50fe701f8
James6588 <75731768+James6588@users.noreply.github.com>
2022-12-22 20:13:38 +0800

    replace  empty.world with smalltown.world

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,11 +2,12 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
-
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
@@ -17,6 +18,16 @@ def generate_launch_description():
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
     package_name='nsbot' 
+    pkg_share = FindPackageShare(package='nsbot').find('navbot')    
+    world_file_name = 'empty.world'
+    world_path = os.path.join(pkg_share, 'worlds', world_file_name)
+
+    world = LaunchConfiguration('world')
+
+    declare_world_cmd = DeclareLaunchArgument(
+      name='world',
+      default_value=world_path,
+      description='Full path to the world model file to load')
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -30,7 +41,7 @@ def generate_launch_description():
     start_gazebo_server_cmd = IncludeLaunchDescription(
       PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzserver.launch.py')]),
       #condition=IfCondition(use_simulator),
-      #launch_arguments={'world': world}.items()
+      launch_arguments={'world': world}.items()
     )
 
     # Start Gazebo client    
@@ -49,6 +60,7 @@ def generate_launch_description():
 
     # Launch them all!
     return LaunchDescription([
+        declare_world_cmd,
         rsp,
         start_gazebo_server_cmd,
         start_gazebo_client_cmd,
